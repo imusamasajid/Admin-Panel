b96ec167cbaa96530d595e3716bb194f4261326f
Muhammad Usama <usama41555@gmail.com>
2023-07-16 08:29:48 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.scss";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Auth from "./auth";
+import Screen from "./screen";
+
+function App() {
+  return (
+    <>
+      {/* <Auth /> */}
+      <Screen />
+    </>
+  );
+}
+
+export default App;
